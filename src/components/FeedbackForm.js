66785b366e63ca56e7be9e0b16de2464db90795e
Helena Johansson <helena@klarr.se>
2022-03-15 13:20:22 +0100

    finished function fro adding feedback

---
+++ src/components/FeedbackForm.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import Card from "./Card";
+import Button from "./Button";
+import RatingSelect from "./RatingSelect";
+
+const FeedbackForm = ({ handleAdd }) => {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text message must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+
+      handleAdd(newFeedback);
+      setText("");
+    }
+  };
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you like to rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Write a review"
+            value={text}
+            onChange={handleTextChange}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
+};
+
+export default FeedbackForm;
