8e12f9dc96b747f2be83723ea7d703ab632fdb17
Helena Johansson <helena@klarr.se>
2022-03-15 09:30:19 +0100

    code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+
 function App() {
-  return <div>Ratings App</div>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
+  );
 }
 
 export default App;
+
+// 1. skapa en header
+// 2. skapa FeedbackItem: representera ett omdöme och en score
+// 3. skapa FeedbackList: ska visa alla feedbackitems
